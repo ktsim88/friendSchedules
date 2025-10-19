3ddac30059acb6a063d55aa4c4b48b2e88ae52a9
ktsim88 <526ktsim@frhsd.com>
2025-10-19 21:16:40 +0000

    finished making layout

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ async function loadSchedule () {
 	try {
 		const schedule = await fetch("kristen.json");
 		const data = await schedule.json();
-		
-    data.forEach(element => {
-        document.getElementById("schedules").innerHTML += `
-        <h2>${element.className}</h2>
-        <p>${element.teacher}</p>
-        <p>${element.roomNumber}</p>
-        <p>${element.period}</p>
-        <p>${element.subjectArea}</p>
-        `
+		//for each loop that gets the objects from json files and displays them
+    data.forEach(schedule => {
+        const html =`
+       <div class="col-sm-12 col-md-4">
+                <div class="card my-3 py-2">
+  <div class="card-header">
+    Period ${schedule.period}
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">${schedule.className}</h5>
+    <p class="card-text">Teacher: ${schedule.teacher} <br> Room: ${schedule.roomNumber} <br> Subject: ${schedule.subjectArea}</p>
+  </div>
+</div>
+            </div>`
+        document.getElementById("schedules").insertAdjacentHTML("beforeend", html);
     });
 
 } catch (err) {
-	document.getElementById("schedules").textContent = "Network error:" + err;
+	document.getscheduleById("schedules").textContent = "Network error:" + err;
 
 }
 
