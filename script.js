2ecb440e7e040f51a40c49de6f43676880186b77
ktsim88 <526ktsim@frhsd.com>
2025-10-21 17:48:35 +0000

    decorated website and added js comments

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
+//arrays for event listener
 const schedules = ["kristen.json", "nandika.json", "shagan.json", "yasmina.json"];
 let scheduleIndex = 0;
 const names = ["Kristen", "Nandika", "Shagan", "Yasmina"];
 let nameIndex = 0;
-
+//async function
 async function loadSchedule() {
+  // this will try to get the json files
   try {
+  //will catch json file
     const res = await fetch(`json/${schedules[scheduleIndex]}`);
     const data = await res.json();
-
+    //container var
     const container = document.getElementById("schedulesSection");
     container.innerHTML = ""; // clear previous content
-
+// using a for each loop to display all cards
     data.forEach(schedule => {
       const html = `
-        <div class="col-sm-12 col-md-4">
-          <div class="card my-3 py-2">
+        <div class="col-sm-12 col-md-4 my-3 py-1">
+          <div class="card h-100">
             <div class="card-header">Period ${schedule.period}</div>
             <div class="card-body">
               <h5 class="card-title">${schedule.className}</h5>
               <p class="card-text">
-                Teacher: ${schedule.teacher} <br>
-                Room: ${schedule.roomNumber} <br>
-                Subject: ${schedule.subjectArea}
+                <b>Teacher:</b> ${schedule.teacher} <br>
+                <b>Room:</b> ${schedule.roomNumber} <br>
+                <b>Subject:</b> ${schedule.subjectArea}
               </p>
             </div>
           </div>
         </div>`;
+      //builds each card into container
       container.insertAdjacentHTML("beforeend", html);
     });
+    //catches error
   } catch (err) {
     document.getElementById("schedulesSection").textContent =
-      "Network error: " + err;
+      `<p>Network Error: ${err}</p>`;
   }
 }
-
+// id for double click section
 const dblClickSection = document.getElementById("doubleClickSection");
 
 dblClickSection.addEventListener("dblclick", () => {
-  // Increment indices first
+  //increment 
   scheduleIndex++;
   nameIndex++;
 
-  // Reset if past the end of arrays
-  if (scheduleIndex >= schedules.length){ scheduleIndex = 0;
-      
+  // reset to index 0 after it hits the array's length
+  if (scheduleIndex >= schedules.length) {
+    scheduleIndex = 0;   
   }
   if (nameIndex >= names.length){ 
       nameIndex = 0;
-      
   }
 
-  // Update displayed name
+  // update  name
   document.getElementById("scheduleName").textContent = 
     `You are viewing ${names[nameIndex]}'s Schedule`;
 
-  // Load the schedule for the current index
+  // load the schedule for the current index
   loadSchedule();
 });
 
-
+//load current schedule
 loadSchedule();
